18bfbe77b6bf765e6a84c9ddd17da40fbc70ccf5
syadav15 <syadav15@LIN20000304>
2017-06-27 12:31:29 +0530

    First time Code Commit

---
+++ AgeCalculate/AgeCalculate/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AgeCalculate
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void bt_click(object sender, EventArgs e)
+        {
+            
+            string _Age = CalculateYourAge(dtdob.Date);
+            lblAge.Text = _Age;
+        }
+        //sky Commit
+        public static string CalculateYourAge(DateTime Dob)
+        {
+            DateTime Now = DateTime.Now;
+            int _Years = new DateTime(DateTime.Now.Subtract(Dob).Ticks).Year - 1;
+            DateTime _DOBDateNow = Dob.AddYears(_Years);
+            int _Months = 0;
+            for (int i = 1; i <= 12; i++)
+            {
+                if (_DOBDateNow.AddMonths(i) == Now)
+                {
+                    _Months = i;
+                    break;
+                }
+                else if (_DOBDateNow.AddMonths(i) >= Now)
+                {
+                    _Months = i - 1;
+                    break;
+                }
+            }
+            int Days = Now.Subtract(_DOBDateNow.AddMonths(_Months)).Days;
+
+            return $"Age is {_Years} Years {_Months} Months {Days} Days";
+        }
+
+        
+    }
+}
